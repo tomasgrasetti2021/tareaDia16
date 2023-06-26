85e5c2b6b062832cc7f69cf5292e7297c91df732
tomasgrasetti2021 <136123948+tomasgrasetti2021@users.noreply.github.com>
2023-06-26 14:57:45 -0600

    Add files via upload

---
+++ TareaIfDia16.java
@@ -0,0 +1,22 @@
+package tareadia16;
+import java.util.Scanner;
+public class TareaIfDia16 {
+	public static void main(String [] args) {
+		
+		Scanner en = new Scanner(System.in);
+		
+		System.out.println("ingrese numero: ");
+		int numero = en.nextInt();
+		
+		if (numero < 0) {
+			System.out.println("el numero es negativo");
+			
+		} else if (numero == 0) {
+			System.out.println("El numero es 0");
+		} else { System.out.println("el numero es posotivo");
+		
+		
+	}
+
+	
+}}
